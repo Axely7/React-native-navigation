31a8685ecfd170299363a2c33e7b2c15a10d2f0e
Axel Jimenez <axely7@hotmail.com>
2023-03-08 09:40:17 -0600

    bottom tabs for ios and android

--- src/navigator/Tabs.tsx
+++ src/navigator/Tabs.tsx
@@ -1,44 +1,118 @@
 import React from 'react';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Tab1Screen} from '../screens/Tab1Screen';
 import {Tab2Screen} from '../screens/Tab2Screen';
 import {StackNavigator} from './StackNavigator';
-
-const Tab = createMaterialBottomTabNavigator();
+import {Platform, Text} from 'react-native';
+import {colores} from '../theme/appTheme';
 
 export const Tabs = () => {
-  return (
-    <Tab.Navigator
-      activeColor="#2C3F73"
-      barStyle={{backgroundColor: 'white'}}
-      screenOptions={({route}) => ({
-        tabBarLabelStyle: {
-          fontSize: 20,
-        },
-        tabBarIcon: ({color, size}) => {
-          const icons = {
-            Home: 'home',
-            Profile: 'account',
-          };
+  return Platform.OS === 'ios' ? <TabsIOS /> : <TabsAndroid />;
+};
 
-          return '';
+const BottomTabAndroid = createMaterialBottomTabNavigator();
+
+export const TabsAndroid = () => {
+  return (
+    <BottomTabAndroid.Navigator
+      sceneAnimationEnabled={true}
+      barStyle={{backgroundColor: colores.primary}}
+      screenOptions={({route}) => ({
+        tabBarActiveTintColor: colores.primary,
+        tabBarLabelStyle: {
+          fontSize: 40,
+        },
+        tabBarIcon: ({color, focused}) => {
+          let iconName: string = '';
+          switch (route.name) {
+            case 'Tab1Screen':
+              iconName = 'T1';
+              break;
+            case 'Tab2Screen':
+              iconName = 'T2';
+              break;
+            case 'StackNavigator':
+              iconName = 'St';
+              break;
+          }
+
+          return <Text style={{color}}>{iconName}</Text>;
         },
       })}>
-      <Tab.Screen
+      <BottomTabAndroid.Screen
         name="Tab1Screen"
-        options={{title: 'Tab1'}}
+        options={{
+          title: 'Tab1',
+        }}
         component={Tab1Screen}
       />
-      <Tab.Screen
+      <BottomTabAndroid.Screen
         name="Tab2Screen"
         options={{title: 'Tab2'}}
         component={Tab2Screen}
       />
-      <Tab.Screen
+      <BottomTabAndroid.Screen
         name="StackNavigator"
         options={{title: 'Stack'}}
         component={StackNavigator}
       />
-    </Tab.Navigator>
+    </BottomTabAndroid.Navigator>
+  );
+};
+
+const BottomTabIOS = createBottomTabNavigator();
+
+export const TabsIOS = () => {
+  return (
+    <BottomTabIOS.Navigator
+      screenOptions={({route}) => ({
+        tabBarActiveTintColor: colores.primary,
+        tabBarLabelStyle: {
+          fontSize: 15,
+        },
+        tabBarIcon: ({color, size, focused}) => {
+          let iconName: string = '';
+          switch (route.name) {
+            case 'Tab1Screen':
+              iconName = 'T1';
+              break;
+            case 'Tab2Screen':
+              iconName = 'T2';
+              break;
+            case 'StackNavigator':
+              iconName = 'St';
+              break;
+          }
+
+          return <Text style={{color}}>{iconName}</Text>;
+        },
+      })}>
+      {/* <Tab.Screen
+        name="Tab1Screen"
+        options={{
+          title: 'Tab1',
+          tabBarIcon: props => <Text style={{color: props.color}}>T1</Text>,
+        }}
+        component={Tab1Screen}
+      /> */}
+      <BottomTabIOS.Screen
+        name="Tab1Screen"
+        options={{
+          title: 'Tab1',
+        }}
+        component={Tab1Screen}
+      />
+      <BottomTabIOS.Screen
+        name="Tab2Screen"
+        options={{title: 'Tab2'}}
+        component={Tab2Screen}
+      />
+      <BottomTabIOS.Screen
+        name="StackNavigator"
+        options={{title: 'Stack'}}
+        component={StackNavigator}
+      />
+    </BottomTabIOS.Navigator>
   );
 };
